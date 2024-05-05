87332f7227ce74046e50a7f9ea2310a10656d06e
bharath varma <you@example.com>
2024-05-05 17:25:02 +0530

    for Jenkins Section 7 Poll SCM

---
+++ OddSquares.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.File;
+
+public class OddSquares
+{
+
+public static void main(String args[])
+{
+	ArrayList<Integer> l= new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(12);
+		l.add(5);
+		l.add(3);
+		l.add(97);
+		
+	l.stream().filter(odd -> odd%2!=0).map(odd -> odd*odd).forEach(System.out :: println);
+}
+}
